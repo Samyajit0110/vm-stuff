4cba8de1ba3dde630be29b5fef8557d8e043edfd
samyajit0110 <samyajitpoddar@gmail.com>
2023-02-10 20:30:13 +0530

    gg

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -1,3 +1,17 @@
 #include<stdio.h>
+#include<string.h>
+int is_auth(char* pass)
+{
 
+}
+void main(int argc, char *argv[])
+{
+    int value = 0;
+    char got_pass[4];
+    strcpy(got_pass,argv[1]);
+    printf("heyo\n");
+    printf("%d\n",argc);
+    printf("%d\n",value);
+    puts(argv[1]);
+}
 
